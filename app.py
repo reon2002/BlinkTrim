156b777ef8a21ecfb8260ac171306b38e01e43f8
Reon Augustine <augustinereon@gmail.com>
2023-09-06 15:25:03 +0530

    new commit

--- app.py
+++ app.py
@@ -53,4 +53,4 @@
 from Project import app  # Update this import statement to match your package name
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=5000,debug=True)
